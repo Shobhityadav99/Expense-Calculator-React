ed7089800acaefa938a8f190acc5465a073f050f
Shobhit Yadav <shobhit999000@gmail.com>
2021-04-29 13:19:57 +0530

    Handlers in progress

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React , { useState } from 'react';
 import './ExpenseForm.css'
 const ExpenseForm = () => {
+    const [enteredTitle,setenteredTitle] =useState('');
+    const [enteredAmount,setenteredAmount] =useState(0.0);
+    const [enteredDate,setenteredDate] =useState('');
     const titleChangeHandler = (event) => {
-        
+        setenteredTitle(event.target.value);
+    }
+    const amountChangeHandler = (event) => {
+        setenteredAmount(event.target.value);
+    }
+    const dateChangeHandler = (event) => {
+        setenteredDate(event.target.value);
     }
     return <form>
         <div className="new-expense__controls">
@@ -12,11 +21,11 @@ const ExpenseForm = () => {
             </div>
             <div className="new-expense__control">
                 <label>Amout</label>
-                <input type="number" min="0.01" step="0.01"/>
+                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31" />
+                <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
             </div>
         </div>
         <div className="new-expense__actions">
