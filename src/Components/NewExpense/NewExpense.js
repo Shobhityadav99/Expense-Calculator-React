99ac0f1ae84aa26e20af3e370488cd102f43ec5f
Shobhit Yadav <shobhit999000@gmail.com>
2021-04-29 14:04:33 +0530

    Adding done

--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
-const NewExpense = () => {
+const NewExpense = (props) => {
     const saveExpenseDataHandler = (enteredExpensedata) => {
         const expenseData ={
             ...enteredExpensedata,
             id: Math.random().toString()
         };
-        console.log(expenseData);
+        // console.log(expenseData);
+        props.onAddExpense(expenseData);
     }
     return <div className="new-expense">
         <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
