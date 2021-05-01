223b84b6632f4b3adaa37c28e714bb2d9ed5290d
Shobhit Yadav <shobhit999000@gmail.com>
2021-05-01 19:29:03 +0530

    styling adding

--- src/Components/Chart/ChartBar.js
+++ src/Components/Chart/ChartBar.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './ChartBar.css';
 
 const ChartBar = props => {
-    return 
+    return <div className="chart-bar">
+        <div className="chart-bar__inner">
+            <div className="chart-bar__fill"></div>
+        </div>
+            <div className="chart-bar__label"></div>
+    </div>
 }
 
 export default ChartBar;
